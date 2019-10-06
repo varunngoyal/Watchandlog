c02585917a12a08034c10cf623f48021b2776f47
anirudh_r_hub <anirudhramani16@gmail.com>
2019-10-06 22:06:24 +0530

    option page

---
+++ app/src/main/java/com/example/watchandlog/option_page.java
@@ -0,0 +1,48 @@
+package com.example.watchandlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class option_page extends AppCompatActivity {
+
+    private Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_page);
+    }
+
+    public void goto_page(View view)
+    {
+        final Intent intent;
+        Button balance_sheet = (Button) findViewById(R.id.balance_sheet);
+        Button add_data = (Button) findViewById(R.id.add_data);
+
+        if(view==balance_sheet)
+        {
+            intent = new Intent(this, balance_sheet.class);
+            Bundle bundle = getIntent().getExtras();
+            intent.putExtras(bundle);
+
+            finish();
+            startActivity(intent);
+        }
+        else
+        {
+            intent = new Intent(this, anu_page.class);
+            Bundle bundle = getIntent().getExtras();
+            intent.putExtras(bundle);
+            finish();
+            startActivity(intent);
+
+        }
+    }
+}
